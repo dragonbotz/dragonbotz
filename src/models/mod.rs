544100f4d11cd6a7272f54a85d56bb6bfdd9a366
DrLarck <me@lahventure.org>
2026-07-26 14:23:30 +0200

    feat: add models

---
+++ src/models/mod.rs
@@ -0,0 +1,49 @@
+//! dragonbotz - The Dragon Ball text-based gacha game
+//! Copyright (C) 2019  Crocomango
+//!
+//! This program is free software: you can redistribute it and/or modify
+//! it under the terms of the GNU General Public License as published by
+//! the Free Software Foundation, either version 3 of the License, or
+//! (at your option) any later version.
+//!
+//! This program is distributed in the hope that it will be useful,
+//! but WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//! GNU General Public License for more details.
+//!
+//! You should have received a copy of the GNU General Public License
+//! along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//! ---
+//! Declaration and implementation of serializables
+
+/// Represents a rarity
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+pub enum Rarity {
+    Common,
+    Uncommon,
+    Super,
+    Extreme,
+    Ultra,
+    Kami,
+}
+
+/// Represents a dragonbotz character
+pub struct Character<'a> {
+    name: &'a str,
+    rarity: Rarity,
+}
+
+impl<'a> Character<'a> {
+    /// Creates a new character
+    pub fn new(name: &'a String, rarity: Rarity) -> Self {
+        Character { name, rarity }
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn rarity(&self) -> &Rarity {
+        &self.rarity
+    }
+}
